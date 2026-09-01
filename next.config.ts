f722ebb7818c9bfe91f6d7ed01a51a4a5a6d0fe5
Santi Salas Cuna <santiucqcba@gmail.com>
2026-09-01 16:43:44 -0300

    corregimos error de las imagenes en github?

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "";
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || repo || "",
   images: {
     unoptimized: true,
   },
